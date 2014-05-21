871c3261ed8507cc3697f44c07d7689de4bc0eb5
Alexandre Coninx <a.coninx@imperial.ac.uk>
2014-05-21 17:29:42 +0100

    Test python code

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Alexandre Coninx
+    Imperial College London, 21/05/2014
+""" 
+
+import numpy as np
+from nite2 import *
+
+import time
+
+
+
+NiTE.initialize()
+
+tracker = UserTracker()
+tracker.create()
+
+while(True):
+	f = tracker.readFrame()
+	for uid in f.getUsers():
+		user = f.getUserById(uid)
+		if user.isNew():
+			print("New user id %d !" % uid)
+			tracker.startSkeletonTracking(uid)
+		else:
+			print(user.getSkeleton())
+			
+
+tracker.destroy()
+NiTE.shutdown()
