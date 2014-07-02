8a64b8a8052d99461c34b4f94ce3cc0820c189d5
Alexandre Coninx <a.coninx@imperial.ac.uk>
2014-07-02 12:48:47 +0100

    Horrible bug that prevented from getting position

--- skeletonjoint_b.cpp
+++ skeletonjoint_b.cpp
@@ -28,9 +28,9 @@ py::list SkeletonJoint_b::getOrientation()
 py::list SkeletonJoint_b::getPosition()
 {
 	py::list l;
-	l.append(joint->getOrientation().x);
-	l.append(joint->getOrientation().y);
-	l.append(joint->getOrientation().z);
+	l.append(joint->getPosition().x);
+	l.append(joint->getPosition().y);
+	l.append(joint->getPosition().z);
 	return l;
 }
 
